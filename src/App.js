26acf1e89a51c75aa79dd462953ef0b6b6a50302
Srutip04 <subuhishruti75@gmail.com>
2022-08-06 11:37:11 +0530

    setting logitude and latitude

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import * as tt from "@tomtom-international/web-sdk-maps";
 
 function App() {
@@ -13,14 +14,40 @@ function App() {
     let map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
       container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
       center: [mapLongitude, mapLatitude],
       zoom: mapZoom,
     });
     setMap(map);
-  }, []);
+    return () => map.remove();
+  }, [mapLongitude, mapLatitude]);
   return (
     <div className="App">
-      <div ref={mapElement} className="mapDiv"></div>
+      <div ref={mapElement} className="mapDiv" />
+      <div className="search-bar">
+        <h1>Where to?</h1>
+        <input
+          type="text"
+          id="longitude"
+          className="longitude"
+          placeholder="Put in Longitude"
+          onChange={(e) => {
+           setMapLongitude(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          id="latitude"
+          className="latitude"
+          placeholder="Put in latitude"
+          onChange={(e) => {
+          setMapLatitude(e.target.value);
+          }}
+        />
+      </div>
     </div>
   );
 }
